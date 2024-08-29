7eef9f212c4fb690b434f23770e488494d000d4a
fedebertazzo <32722235+fedebertazzo@users.noreply.github.com>
2024-08-29 17:34:07 -0300

    Update autos_data.py
    agregado Toyota Corolla negro

--- Proyecto_Garage/autos_data.py
+++ Proyecto_Garage/autos_data.py
@@ -2,5 +2,6 @@
 lista_autos=[
     {"dominio":"AA123ZZ","marca":"Ford","modelo":"Focus","color":"blanco"},
     {"dominio":"BB456YY","marca":"Fiat","modelo":"600","color":"azul"},
-    {"dominio":"CC789XX","marca":"Chevrolet","modelo":"Tracker","color":"gris"}
+    {"dominio":"CC789XX","marca":"Chevrolet","modelo":"Tracker","color":"gris"},
+    {"dominio":"DD789AA","marca":"Toyota","modelo":"Corolla","color":"negro"}
 ]
