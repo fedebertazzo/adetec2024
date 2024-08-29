fd70e4a61e228f7ec696b8147ec27c22b32f0a2d
fedebertazzo <fedebertazzo@gmail.com>
2024-08-29 19:19:18 -0300

    Ejemplo con tkinter

--- Proyecto_Garage/interfaz.py
+++ Proyecto_Garage/interfaz.py
@@ -1,7 +1,10 @@
 import tkinter
 
-def mostrar_mensaje(mensaje):
-    print(mensaje)
+def mostrar_mensaje():
+    #Muestra el texto
+    print(texto1.get())
+    #Limpia el cuadro de texto una vez que se muestra en pantalla
+    texto1.delete(0, tkinter.END)
 
 #Creo el objeto ventana
 ventana=tkinter.Tk()
@@ -9,14 +12,13 @@ ventana.title("Mi garage")
 ventana.geometry("100x100")
 
 #Creo el cuadro de texto
+titulo=tkinter.Label(text="Texto")
 texto1=tkinter.Entry()
+titulo.pack()
 texto1.pack()
 
-
 #Creo el objeto botón
-boton1=tkinter.Button(ventana,text="Click aquí",bg="grey", command=mostrar_mensaje(texto1))
+boton1=tkinter.Button(ventana,text="Click aquí",bg="grey", command=mostrar_mensaje)
 boton1.pack()
-
-
 
 ventana.mainloop()
